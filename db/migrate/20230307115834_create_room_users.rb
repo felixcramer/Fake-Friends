96a57db6d644aabebcf23ad1fa2c68ac607f91ca
Florian Miller <flokomi@web.de>
2023-03-07 13:00:25 +0100

    created models room question room_users and added username to users

---
+++ db/migrate/20230307115834_create_room_users.rb
@@ -0,0 +1,10 @@
+class CreateRoomUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :room_users do |t|
+      t.references :room, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
