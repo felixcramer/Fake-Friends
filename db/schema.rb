10bf8b522aa3aa0042d4dcd505a964ef6e91715b
Felix Cramer <felixcramer18@gmail.com>
2023-03-08 15:27:47 +0100

    user_answers table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_094317) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_141623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,6 +75,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_094317) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.index ["user_id"], name: "index_rooms_on_user_id"
+  end
+
+  create_table "user_answers", force: :cascade do |t|
+    t.integer "answer_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
