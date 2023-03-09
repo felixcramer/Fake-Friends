0ad572bf29498c89432e35198d3a3131d2fa0031
Matias Roje <rojechi@gmail.com>
2023-03-09 23:14:51 +0100

    page-changer fully working to any given set of questions

--- app/controllers/room_questions_controller.rb
+++ app/controllers/room_questions_controller.rb
@@ -1,5 +1,10 @@
 class RoomQuestionsController < ApplicationController
   def show
     @room_question = RoomQuestion.find(params[:id])
+    @room_questions = @room_question.room.room_questions
+    @array_of_urls = []
+    @room_questions.each do |question|
+      @array_of_urls << room_room_question_path(question.room, question)
+    end
   end 
 end
--- app/javascript/controllers/change_page_controller.js
+++ app/javascript/controllers/change_page_controller.js
@@ -2,19 +2,25 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="change-page"
 export default class extends Controller {
-  static values = { questionCount: Number };
-  connect() {
-    // setTimeout(() => {
-    //   this.changingPage();
-    // }, 3000);
-    this.questionCountValue;
-  }
+  static values = {
+    questionCount: Array,
+    url: String,
+  };
 
-  changingPage() {
-    let url = window.location.href;
-    let ending = parseInt(url.slice(-1)) + 1;
-    let urlModified = url.substring(0, url.length - 1);
-    const finalUrl = urlModified + ending;
-    window.location = finalUrl;
+  connect() {
+    const length = this.questionCountValue.length;
+    let currentPage = this.questionCountValue.indexOf(
+      window.location.pathname,
+      0
+    );
+    if (currentPage + 1 === length) {
+      setTimeout(() => {
+        window.location = this.urlValue;
+      }, 5000);
+    } else {
+      setTimeout(() => {
+        window.location = this.questionCountValue[currentPage + 1];
+      }, 5000);
+    }
   }
 }
