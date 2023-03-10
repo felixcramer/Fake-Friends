5005095b1c4b52329a163727fafbd22c3fbe7525
Felix Cramer <felixcramer18@gmail.com>
2023-03-10 09:06:06 +0100

    adjusting routes for new controller

--- app/controllers/room_questions_controller.rb
+++ app/controllers/room_questions_controller.rb
@@ -1,5 +1,16 @@
 class RoomQuestionsController < ApplicationController
   def show
     @room_question = RoomQuestion.find(params[:id])
-  end 
+  end
+
+  def create
+    new_answer = RoomQuestion.new(user: current_user, answer: params[:user_answer][:answer])
+    new_answer.save
+  end
+
+  private
+
+  def user_answers_params
+    params.require(:user_answers).permit(:answer_id)
+  end
 end
