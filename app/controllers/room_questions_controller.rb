74826ff6966182a8d4c2959f4de71a7f324e5068
Matias Roje <rojechi@gmail.com>
2023-03-13 23:28:08 +0100

    second questionary started, form returning usersnames and user_ids

--- app/controllers/room_questions_controller.rb
+++ app/controllers/room_questions_controller.rb
@@ -3,30 +3,44 @@ class RoomQuestionsController < ApplicationController
     @users = User.all
     @room = Room.find(params[:room_id])
     @room_question = RoomQuestion.find(params[:id])
-    @room_questions = @room_question.room.room_questions
-    @array_of_urls = []
-    @room_questions.each do |question|
-      @array_of_urls << room_room_question_path(question.room, question)
+
+    if @room_question.question.round == 1
+      @room_questions = @room_question.room.room_questions
+      @array_of_urls = []
+      @room_questions.each do |question|
+        @array_of_urls << room_room_question_path(question.room, question)
+      end
+      @answer = Answer.new
     end
 
-    @answer = Answer.new
+    if @room_question.question.round == 2
+      @user_answers = UserAnswer.where(room_id: @room)
+    end
   end 
 
   def create
   end
 
   def update
-  @room = Room.find(params[:room_id])
-  @room_question = RoomQuestion.find(params[:id])
-  if @room_question.question.round == 1
-    @new_answer = UserAnswer.new
-    @new_answer.room = @room
-    @new_answer.user = current_user
-    @picked_answer = Answer.find(params[:room_question][:answer_ids][1])
-    @new_answer.answer = @picked_answer
-    @new_answer.save
-    redirect_to room_room_question_path(@room, @room_question)
-  end
+    @room = Room.find(params[:room_id])
+    @room_question = RoomQuestion.find(params[:id])
+
+    if @room_question.question.round == 1
+      @new_answer = UserAnswer.new
+      @new_answer.room = @room
+      @new_answer.user = current_user
+      @picked_answer = Answer.find(params[:room_question][:answer_ids][1])
+      @new_answer.answer = @picked_answer
+      @new_answer.save
+      redirect_to room_room_question_path(@room, @room_question)
+    end 
+
+    if @room_question.question.round == 2
+      @user = current_user
+      @picked_user = User.find(params[:room_question][:answer_ids][1])
+      @user_answers = UserAnswer.where(user_id: @user, room_id: @room)
+      raise
+    end 
   end
 
   private
