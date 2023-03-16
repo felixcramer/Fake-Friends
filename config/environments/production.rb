20b73c4f7f4ba5e14af94a27c8f9f235759967f5
Felix Cramer <felixcramer18@gmail.com>
2023-03-16 16:37:16 +0100

    Updated config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  # config.assets.compile = false
-  config.assets.precompile += %w( '.svg' )  
+  config.assets.compile = false
+  config.assets.precompile += %w( '.svg' )
 
   # Must include to get inline SVGs to work in deploy
   config.assets.css_compressor = :sass
