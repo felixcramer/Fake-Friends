90b7059c8d0688ede28f2cbfb14825fc78626fae
Felix Cramer <felixcramer18@gmail.com>
2023-03-16 16:15:36 +0100

    asdfasd

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module RailsAuthentication
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.autoloader = :classic
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
